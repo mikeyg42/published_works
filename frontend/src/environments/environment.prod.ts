b0ac4c609727acd034a394a2f51f0e080ee7e788
Michael Glendinning <mglendin1@gmail.com>
2025-04-24 20:49:34 -0400

    ready to try to deploy to gcp a 3rd time?

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
 export const environment = {
   production: true,
-  
-  // Option 1: Use a relative WebSocket URL (works on any domain)
-  //websocketUrl: 'wss://'+window.location.host+'/maze-solver'
-  
-  // Option 2: Specify a fixed production server
-  websocketUrl: 'wss://maze-solver-backend-acn3zn6u4a-uc.a.run.app/maze-solver'
+  websocketUrl: 'wss://michaelglendinning.com/maze-solver',
 };
